5420c22b1b450d5717a0ecd823fe27dcc6c8fcad
Aleksei Iurin <19alex85@gmail.com>
2022-05-08 16:08:56 +0300

    Initial commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,49 @@
+class QuestionsController < ApplicationController
+  before_action :set_question, only: %i[show edit update destroy hide]
+
+  def create
+    @question = Question.create(question_params)
+
+    redirect_to question_path(@question)
+  end
+
+  def update
+    @question.update(question_params)
+
+    redirect_to question_path(@question)
+  end
+
+  def destroy
+    @question.destroy
+
+    redirect_to questions_path
+  end
+
+  def shows; end
+
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def edit; end
+
+  def hide
+    @question.update(hidden: true)
+
+    redirect_to question_path
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body, :user_id)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+end
