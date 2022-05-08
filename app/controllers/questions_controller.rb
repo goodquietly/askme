31e217a02ca0ef941298f505845e9b70c067e161
Aleksei Iurin <19alex85@gmail.com>
2022-05-08 23:40:41 +0300

    lesson51at the end of lesson 51

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,24 +4,25 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.create(question_params)
 
-    redirect_to question_path(@question)
+    redirect_to question_path(@question), notice: 'Новый вопрос создан!'
   end
 
   def update
     @question.update(question_params)
 
-    redirect_to question_path(@question)
+    redirect_to question_path(@question), notice: 'Обновили вопрос!'
   end
 
   def destroy
     @question.destroy
 
-    redirect_to questions_path
+    redirect_to questions_path, notice: 'Вопрос удален!'
   end
 
   def shows; end
 
   def index
+    @question = Question.new
     @questions = Question.all
   end
 
@@ -34,7 +35,7 @@ class QuestionsController < ApplicationController
   def hide
     @question.update(hidden: true)
 
-    redirect_to question_path
+    redirect_to question_path, notice: 'Скрыли вопрос!'
   end
 
   private
