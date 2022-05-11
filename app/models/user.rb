9444db559e297495b8e0f7194daaed417bc4d988
Aleksei Iurin <19alex85@gmail.com>
2022-05-11 12:48:35 +0300

    53 lesson completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+
+  before_save :downcase_nickname
+
+  validates :email, presence: true, uniqueness: true
+  # uniqueness: { case_sensitive: false }, length: { maximum: 50 },
+  # format: { with: ConstantData::VALID_EMAIL_REGEX }, if: :method_name?
+
+  def downcase_nickname
+    nickname.downcase!
+  end
 end
