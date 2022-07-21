eb651aa0959e46ce627e5cf75c15cf4ef9dc27a4
Aleksei Iurin <19alex85@gmail.com>
2022-07-21 18:01:48 +0300

    hashtag model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_21_112835) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_21_145019) do
+  create_table "hashtags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_hashtags_on_name", unique: true
+  end
+
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
